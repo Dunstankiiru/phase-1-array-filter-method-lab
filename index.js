15d9b23cb90aab7eba8303baed5f777ce243f8c6
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-12 13:12:54 +0300

    completes

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+// Example Usage
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+console.log(findMatching(["Bobby", "Sammy", "Sally", "Annette"], "bobby")); // ["Bobby"]
+console.log(fuzzyMatch(["Bobby", "Sammy", "Sally", "Annette"], "Sa")); // ["Sammy", "Sally"]
+console.log(matchName(drivers, "Bobby"));
+// [{ name: "Bobby", hometown: "New York" }, { name: "Bobby", hometown: "Denver" }]
